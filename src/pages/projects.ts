49e87f0a5f6a2dd1597d5c6275b02c0f74c10fd0
Matias Díaz <matidiaz00@gmail.com>
2025-04-04 03:15:13 -0300

    add content

---
+++ src/pages/projects.ts
@@ -0,0 +1,46 @@
+export const projects = [
+  { id: 0, url: "#", description: "Rest", title: "Marvel" },
+  { id: 1, url: "#", description: "Rendering", title: "Pokemon" },
+  { id: 2, url: "#", description: "Microfrontend", title: "Anime" },
+  { id: 3, url: "#", description: "Rendering", title: "Rick & Morty" },
+  { id: 4, url: "#", description: "Mobile", title: "Curriculum Card" }
+];
+
+export const projects_old = [
+  {
+    id: 0,
+    url: "https://github.com/matidiaz000/clima",
+    description: "React",
+    title: "Clima"
+  },
+  {
+    id: 1,
+    url: "https://github.com/matidiaz000/pokedex",
+    description: "React",
+    title: "Pokedex"
+  },
+  {
+    id: 2,
+    url: "https://github.com/matidiaz000/cotizar",
+    description: "Angular",
+    title: "Cotizar"
+  },
+  {
+    id: 3,
+    url: "https://github.com/matidiaz000/mercadolibre",
+    description: "React",
+    title: "Mercadolibre"
+  },
+  {
+    id: 4,
+    url: "https://github.com/matidiaz000/auto-sprite",
+    description: "Library",
+    title: "Auto Sprite"
+  },
+  {
+    id: 5,
+    url: "https://github.com/matidiaz000/extended-build-manager",
+    description: "Library",
+    title: "Extended Build"
+  }
+];
